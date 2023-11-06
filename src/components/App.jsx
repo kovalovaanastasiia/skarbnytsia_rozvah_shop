d498a282b67ef613f916b6a47f1d56a251c3152b
kovalovaanastasiia <tokach26@gmail.com>
2023-11-06 22:33:07 +0200

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+    </div>
+  );
+};
